6a1b88b77025de512f6243f064aa3ecd01774475
kunte_ <33470032+kunte0@users.noreply.github.com>
2022-01-05 17:52:01 +0100

    Fixed 👁 🦀

--- popups/lab.js
+++ popups/lab.js
@@ -1,51 +1,23 @@
-/// gen HTML for lab.html
-/**
-
-categories = ['sql-injection','cross-site-scripting','csrf','clickjacking','cors',''dom-based,'xxe','ssrf','request-smuggling','os-command-injection','server-side-template-injection','file-path-traversal','access-control','authentication','websockets','web-cache-poisoning','deserialization','information-disclosure','logic-flaws','host-header','oauth','file-upload']
-
-HTMLString = ''
-
-function getName(string){
-    // want to do meow-catz into Meow Catz
-    string = string.replace(/-([a-z])/g, function (g) { return " " + g[1].toUpperCase(); });
-    return string[0].toUpperCase() + string.substring(1)
+function submithint() {
+    console.log(`Submitting Hint request with ${document.getElementById('cat').value}`)
+    browser.runtime.sendMessage({
+        type: 'hint',
+        hint: document.getElementById('cat').value
+    });
 }
 
-categories.forEach(curr => {
-    Options_HTML = `<option value="${curr}">${getName(curr)}</option>`
-    HTMLString += Options_HTML
-});
+function reset() {
+    browser.browserAction.setPopup({
+        popup: browser.runtime.getURL('popups/popup.html')
+    })
+    window.close()
+}
 
-console.log(HTMLString)
- 
-*/
-
-function listenForClicks() {
-    document.addEventListener("click", (e) => {
-  
-
-    function submithint() {
-        console.log(`Submitting Hint request with ${document.getElementById("cat").value}`)
-        browser.runtime.sendMessage({
-            type: 'hint',
-            hint: document.getElementById("cat").value
-          });
-     }
-  
-      function reset() {
-        browser.browserAction.setPopup({popup: browser.runtime.getURL('popups/popup.html')})
-        window.close()
-      }
- 
-      if (e.target.classList.contains("hint")) {
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('hint')) {
         submithint()
-      }
-
-        else if (e.target.classList.contains("reset")) {
+    } else if (e.target.classList.contains('reset')) {
         reset()
- }
+    }
 
-    });
-  }
-  
-listenForClicks()
+});
